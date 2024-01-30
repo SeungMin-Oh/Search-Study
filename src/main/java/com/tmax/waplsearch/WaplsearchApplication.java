b297f4067b31d011b1f3f7cd66009404d54b2d8f
jaewon_park2 <jaewon_park@tmax.co.kr>
2024-01-30 15:24:50 +0900

    Initial commit

---
+++ src/main/java/com/tmax/waplsearch/WaplsearchApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.waplsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaplsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaplsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmax/waplsearch/WaplsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.waplsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaplsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
